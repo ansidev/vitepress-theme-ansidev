4d28d447700b64a6b40108c051762786ce5264f5
ansidev <ansidev@gmail.com>
2024-12-28 16:02:52 +0700

    fix(composable): sort object properties by count value

--- src/vitepress/composables/rank.ts
+++ src/vitepress/composables/rank.ts
@@ -1,11 +1,13 @@
 export const useRank = (arr: string[]) => {
-  const rank: Record<string, number> = arr
-    .reduce((acc: Record<string, number>, str: string) => {
+  const rank: Record<string, number> = arr.reduce(
+    (acc: Record<string, number>, str: string) => {
       acc[str] = (acc[str] || 0) + 1 // Increment the count if the key exists, otherwise initialize to 1
       return acc
-    }, {})
+    },
+    {}
+  )
 
   return Object.entries(rank)
-    .sort(([,a],[,b]) => b-a)
+    .sort(([, a], [, b]) => b - a)
     .reduce((r, [k, v]) => ({ ...r, [k]: v }), {})
 }
