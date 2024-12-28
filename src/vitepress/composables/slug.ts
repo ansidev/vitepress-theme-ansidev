73e54fa5c35e29044afa18bca4f6b7c22b8822d2
ansidev <ansidev@gmail.com>
2024-12-28 16:03:20 +0700

    chore(composable): reformat code

--- src/vitepress/composables/slug.ts
+++ src/vitepress/composables/slug.ts
@@ -1,10 +1,10 @@
 export const useSlug = (str: string) => {
   const s = str.replace(/#/, '-sharp-').replace(/\./, '-dot-').replace(/-$/, '')
   return s
-  .toLowerCase()
-  .normalize('NFD') // Decompose Unicode characters
-  .replace(/[\u0300-\u036f]/g, '') // Remove diacritical marks
-  .replace(/đ/g, 'd') // Handle Vietnamese "đ"
-  .replace(/[^a-z0-9]+/g, '-') // Replace non-alphanumeric characters with dashes
-  .replace(/^-+|-+$/g, ''); // Remove leading or trailing dashes
+    .toLowerCase()
+    .normalize('NFD') // Decompose Unicode characters
+    .replace(/[\u0300-\u036f]/g, '') // Remove diacritical marks
+    .replace(/đ/g, 'd') // Handle Vietnamese "đ"
+    .replace(/[^a-z0-9]+/g, '-') // Replace non-alphanumeric characters with dashes
+    .replace(/^-+|-+$/g, '') // Remove leading or trailing dashes
 }
