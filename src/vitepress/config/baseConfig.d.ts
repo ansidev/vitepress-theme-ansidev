dc2644bae836d76f70ceba27e3ce1e6f667577b8
ansidev <ansidev@gmail.com>
2025-02-02 12:41:30 +0700

    fix: update typescript configs

--- src/vitepress/config/baseConfig.d.ts
+++ src/vitepress/config/baseConfig.d.ts
@@ -1,5 +1,5 @@
 import { UserConfig } from 'vitepress'
-import { type Config } from '../config'
+import { type Config } from '../config.js'
 
 declare const config: UserConfig<Config>
 export default config
